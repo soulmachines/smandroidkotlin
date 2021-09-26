69db21644084280ea318c8b085802b4959bdcff2
eugenemanko-soulmachines <eugene.manko@soulmachines.com>
2021-09-27 12:15:46 +1300

    working on the kotlin version of contents awareness api sample code

---
+++ app/src/main/java/com/soulmachines/android/sample/ContentImpl.kt
@@ -0,0 +1,23 @@
+package com.soulmachines.android.sample
+
+import com.soulmachines.android.smsdk.core.scene.Content
+import com.soulmachines.android.smsdk.core.scene.Rect
+
+class ContentImpl(private val bounds: Rect) : Content {
+    private val id = "object-" + Integer.toString(uniqueId++)
+    override fun getId(): String {
+        return id
+    }
+
+    override fun getMeta(): Map<String, Any>? {
+        return null
+    }
+
+    override fun getRect(): Rect {
+        return bounds
+    }
+
+    companion object {
+        var uniqueId = 1
+    }
+}
