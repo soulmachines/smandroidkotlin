b42e58a92dc762b92d7fd1dd40a59658b3586bc8
Dennis Angelo <70665019+dennisangelo-soulmachines@users.noreply.github.com>
2022-07-22 10:38:28 +1200

    License updates and review (#5)
    * updated the license to use one provided by legal team
    
    * adding Copyright to all source codes

--- app/src/main/java/com/soulmachines/android/sample/UnhandledExceptionHandler.java
+++ app/src/main/java/com/soulmachines/android/sample/UnhandledExceptionHandler.java
@@ -1,12 +1,4 @@
-/*
- *  Copyright 2013 The WebRTC Project Authors. All rights reserved.
- *
- *  Use of this source code is governed by a BSD-style license
- *  that can be found in the LICENSE file in the root of the source
- *  tree. An additional intellectual property rights grant can be found
- *  in the file PATENTS.  All contributing project authors may
- *  be found in the AUTHORS file in the root of the source tree.
- */
+// Copyright 2022 Soul Machines Ltd
 
 package com.soulmachines.android.sample;
 
